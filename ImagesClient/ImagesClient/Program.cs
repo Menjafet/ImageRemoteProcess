12c42ad8a304fe45266b5e76fe66f32a4f0b38c5
menjafet <119637452+menjafet@users.noreply.github.com>
2023-01-10 21:54:29 -0600

    Image download great update

---
+++ ImagesClient/ImagesClient/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using htttpLib;
+
+
+namespace baby
+
+{
+    public static class Class2
+    {
+        private static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var folder = @"C:\\Users\\jaftb\\Documents\\ImagesClient\\ImagesClient";
+            var fileName = "test";
+            var url = "https://cdn.discordapp.com/attachments/458291463663386646/592779619212460054/Screenshot_20190624-201411.jpg?query&with.dots";
+
+            await Class1.DownloadImageAsync(folder, fileName, new Uri(url));
+        }
+    }
+}
---
+++ ImagesClient/htttpLib/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace htttpLib
+
+{
+    public static class Class1
+    {
+        public static async Task DownloadImageAsync(string directoryPath, string fileName, Uri uri)
+        {
+            using var httpClient = new HttpClient();
+
+            // Get the file extension
+            var uriWithoutQuery = uri.GetLeftPart(UriPartial.Path);
+            var fileExtension = Path.GetExtension(uriWithoutQuery);
+
+            // Create file path and ensure directory exists
+            var path = Path.Combine(directoryPath, $"{fileName}{fileExtension}");
+            Directory.CreateDirectory(directoryPath);
+
+            // Download the image and write to the file
+            var imageBytes = await httpClient.GetByteArrayAsync(uri);
+            await File.WriteAllBytesAsync(path, imageBytes);
+        }
+    }
+}
